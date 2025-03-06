76dda14c1e1ce387f6f1b3368aad3e2a3238aa78
viitones <vitonesmartins@gmail.com>
2025-03-06 12:20:52 -0300

    cadastrando livros

---
+++ controllers/livro-criar.controller.php
@@ -0,0 +1,34 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD'] !== 'POST') {
+  header('location: /meus-livros');
+  exit();
+}
+
+if(!auth()) {
+  abort(403);
+}
+
+$usuario_id = auth()->id;
+$titulo = $_POST['titulo'];
+$autor = $_POST['autor'];
+$descricao = $_POST['descricao'];
+$ano_de_lancamento = $_POST['ano_de_lancamento'];
+
+$validacao = Validacao::validar([
+  'titulo' => ['required', 'min:3'],
+  'autor' => ['required'],
+  'descricao' => ['required'],
+  'ano_de_lancamento' => ['required']
+], $_POST);
+
+if($validacao->falhou()) {
+  header("location: /meus-livros");
+  exit();
+}
+
+$database->query("insert into livros (usuario_id, titulo, autor, descricao, ano_de_lancamento) values (:usuario_id, :titulo, :autor, :descricao, :ano_de_lancamento)", params: compact('usuario_id', 'titulo', 'autor', 'descricao', 'ano_de_lancamento'));
+
+flash()->push('mensagem', 'Livro cadastrado com sucesso!');
+header('location: /meus-livros');
+exit();
---
+++ controllers/meus-livros.controller.php
@@ -0,0 +1,3 @@
+<?php
+
+view('meus-livros');
